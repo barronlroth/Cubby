0e41efa0f269d18ce43c5ee56e27d4f176d9b727
Barron Roth <barronlroth@gmail.com>
2025-08-16 16:56:19 -0400

    Initial Commit

---
+++ CubbyTests/CubbyTests.swift
@@ -0,0 +1,17 @@
+//
+//  CubbyTests.swift
+//  CubbyTests
+//
+//  Created by Barron Roth on 8/16/25.
+//
+
+import Testing
+@testable import Cubby
+
+struct CubbyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
